abd73a8e3ac30490e9811657bb15c39beb82f7ef
Juan Tenezaca <tenj34@gmail.com>
2018-05-29 22:13:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ function happyHolidays(){
   return "Happy holidays!";
 }
 
-function happyHolidaysTo(name) {
-  return 'Happy holidays, ${name}!';
-}
 
 function happyHolidayTo(holiday,name){
   return `Happy ${holiday}, ${name}!`;
